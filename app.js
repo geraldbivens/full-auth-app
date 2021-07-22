7194d7d6d7cc27e69cc6fa1be5cc77114b3ea5a0
Gerald Bivens <geraldbivens@gmail.com>
2021-07-22 10:57:20 -0600

    Add js to connect the sign up form to the api.

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-"use strict";
+'use strict';
 
-console.log("Hi JS!");
+const signUpForm = document.getElementById('sign-up');
+const baseURL = 'http://localhost:3000/';
+const signUpURL = baseURL + 'users';
+
+signUpForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const username = formData.get('username');
+  const password = formData.get('password');
+  const body = JSON.stringify({
+    username,
+    password,
+  });
+
+  event.target.reset();
+
+  fetch(signUpURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body,
+  });
+});
